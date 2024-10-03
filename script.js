3bae09ffa85e749784309f0d6b7833dbe63677ed
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2024-10-03 11:48:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 // your code here
 function fun()
 {
-	document.getElementById("url").innerHTML=`https://localhost:8080/`
+	var name=document.getElementById("name").value;
+	var year=document.getElementById("year").value;
+    if(name != "" && year != "")
+	    document.getElementById("url").innerHTML=`https://localhost:8080/?name=${name}&year=${year}`
+    else if(name != "")
+	    document.getElementById("url").innerHTML=`https://localhost:8080/?name=${name}`
+    else if(year != "")
+	    document.getElementById("url").innerHTML=`https://localhost:8080/?year=${year}`
+    else
+        document.getElementById("url").innerHTML=`https://localhost:8080/`
 }
